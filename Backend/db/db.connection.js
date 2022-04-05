630f0b18c156b82302ea3ce0b0bf7d213400f6b1
PVIRANI6561 <71581939+PVIRANI6561@users.noreply.github.com>
2022-04-05 14:41:35 +0530

    Delete Backend directory

--- Backend/db/db.connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-// db connection
-mongoose.connect('mongodb://127.0.0.1:27017/skyline', { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-db.on("error", (error) => console.log(error));
-db.once("open", () => console.log("Connected successfully"));
